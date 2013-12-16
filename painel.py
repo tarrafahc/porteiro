0d1d013fee484f6ce9657f0c4303027edd3e9969
Ramiro Polla <ramiro.polla@gmail.com>
2013-12-16 20:53:10 -0200

    script: ler botao, usando timeout como sleep

--- painel.py
+++ painel.py
@@ -4,8 +4,19 @@ import urllib2
 import serial
 import json
 import time
+import os
 
-ser = serial.Serial('/dev/ttyUSB0', 9600)
+def botao():
+    bytes = ser.read(1)
+    if bytes:
+        if   bytes is '1':
+            print "Tarrafa is on"
+            os.system("ssh -i botao-tarrafa tarrafahc@tarrafa.net 1")
+        elif bytes is '0':
+            print "Tarrafa is off"
+            os.system("ssh -i botao-tarrafa tarrafahc@tarrafa.net 0")
+
+ser = serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=1)
 
 time.sleep(1)
 
@@ -20,13 +31,13 @@ while True:
 
     if text != "":
         ser.write(text)
-        time.sleep(5)
+        botao()
 
     lt = time.localtime()
     ser.write("  %02d:%02d:%02d" % (lt.tm_hour, lt.tm_min, lt.tm_sec))
-    time.sleep(5)
+    botao()
     ser.write("%02d/%02d/%4d" % (lt.tm_mday, lt.tm_mon, lt.tm_year))
-    time.sleep(5)
+    botao()
 
 #    url = 'http://api.openweathermap.org/data/2.5/weather?q=florianopolis'
 #    temp = json.loads(urllib2.urlopen(url).read())['main']['temp']-273.15
