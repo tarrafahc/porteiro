7a2eac9d218d18415532ae124a4f522c128f58e9
Ramiro Polla <ramiro.polla@gmail.com>
2014-01-05 02:07:27 -0200

    painel: documentar código

--- painel.py
+++ painel.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import urllib2
 import serial
@@ -6,6 +7,7 @@ import json
 import time
 import os
 
+# Lê um byte da serial pelo Arduino. Se tiver algum dado, atualiza o servidor.
 def botao():
     bytes = ser.read(1)
     if bytes:
@@ -16,11 +18,17 @@ def botao():
             print "Tarrafa is off"
             os.system("ssh -i botao-tarrafa tarrafahc@tarrafa.net 0")
 
+# Abrir a porta serial do Arduino em 9600 baud com tempo de espera de 1
+# segundo. O tempo de espera é usado na função de leitura para não precisar
+# fazer o código dormir explicitamente.
 ser = serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=1)
 
+# Esperar 1 segundo caso o Arduino queira reiniciar.
 time.sleep(1)
 
+# Loop principal do painel.
 while True:
+    # Mostrar texto de arquivo
     text = ""
     try:
         fp = open('text', 'r')
@@ -33,12 +41,16 @@ while True:
         ser.write(text)
         botao()
 
+    # Mostrar hora
     lt = time.localtime()
     ser.write("  %02d:%02d:%02d" % (lt.tm_hour, lt.tm_min, lt.tm_sec))
     botao()
+    # Mostrar data
     ser.write("%02d/%02d/%4d" % (lt.tm_mday, lt.tm_mon, lt.tm_year))
     botao()
 
+    # Mostrar temperatura
+    # TODO pegar uma vez por hora ou coisa parecida
 #    url = 'http://api.openweathermap.org/data/2.5/weather?q=florianopolis'
 #    temp = json.loads(urllib2.urlopen(url).read())['main']['temp']-273.15
 #    ser.write("   %.4g C" % (temp))
